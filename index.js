b378c95c920cdeec396f035141ef7fbc92ba12ff
Brian Illes <brianrilles@gmail.com>
2019-03-18 11:25:04 -0700

    initial

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require("express");
+
+const db = require("./data/db.js");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("working");
+});
+
+server.listen(4000, () => {
+  console.log("\n ** API up and running on port 4k  **");
+});
