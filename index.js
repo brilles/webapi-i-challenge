701204c035f59bef2753b1163a58e67ef9b45975
Brian Illes <brianrilles@gmail.com>
2019-03-18 13:09:24 -0700

    POST, GET /api/users

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 // implement your API here
 const express = require("express");
-
 const db = require("./data/db.js");
-
 const server = express();
+server.use(express.json());
 
-server.get("/", (req, res) => {
-  res.send("working");
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (user.name && user.bio) {
+    db.insert(user)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: {
+            error: "There was an error while saving the user to the database"
+          }
+        });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
 });
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
+    });
+});
+
+// server.get('/', (req, res) => {
+
+// });
+
+// server.delete('/', (req, res) => {
+
+// });
+
+// server.put('/', (req, res) => {
+
+// });
 
 server.listen(4000, () => {
   console.log("\n ** API up and running on port 4k  **");
