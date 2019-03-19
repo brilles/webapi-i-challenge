2aabb6b22e57401b6be6e393adabba49445cbdca
Brian Illes <brianrilles@gmail.com>
2019-03-18 20:35:24 -0700

    initial

--- users/src/App.js
+++ users/src/App.js
@@ -1,87 +1,114 @@
-import React, { Component } from "react";
-import axios from "axios";
-import "./App.css";
-import UserList from "./UserList";
-import UserForm from "./UserForm";
+// import React, { Component } from "react";
+// import axios from "axios";
+// import "./App.css";
+// import UserList from "./UserList";
+// import UserForm from "./UserForm";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: []
-    };
-  }
+import React, {useState} from "react";
 
-  fetchUsers() {
-    axios
-      .get("http://localhost:4000/api/users")
-      .then(res => {
-        this.setState({ users: res.data });
-      })
-      .catch(err => {
-        this.setState({ error: err });
-        console.log(err);
-      });
-  }
-
-  deleteUser = (e, id) => {
-    e.preventDefault();
-    axios
-      .delete(`http://localhost:4000/api/users/${id}`)
-      .then(res => {
-        this.fetchUsers();
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
-  addUser = (name, bio) => {
-    const newUser = {
-      "name": name,
-      "bio": bio
-    };
-    axios
-    .post("http://localhost:4000/api/users", newUser)
-    .then(res => {
-        // console.log(res)
-        // this.setState({ users: [...this.state.users, res.data]})
-        this.fetchUsers();
-    })
-        .catch(err => {
-        console.log(err);
-      });
-   
-  };
-
-  updateUser = (name, bio, id) => {
-    const updatedUser = {
-      "name": name,
-      "bio": bio
-    };
-    axios
-      .put(`http://localhost:4000/api/users/${id}`, updatedUser)
-      .then(res => {
-        this.fetchUsers();
-      })
-      .catch(err => {
-        console.log(err);
-      });
-
-  };
-
-  componentDidMount() {
-    this.fetchUsers();
-  }  
-
-  render() {
-    return (
-      <div className="App">
-        <UserForm addUser={this.addUser} updateUser={this.updateUser}/>
-        <UserList users={this.state.users} deleteUser={this.deleteUser} />
-      </div>
-    );
-  }
+function UserList({user, deleteUser}) {
+  return(
+    <p>hello from the userList</p>
+  )
 }
 
-export default App;
+function UserForm({ addUser, updateUser }) {
+  return (
+    <p>hello from the userform</p>
+  )
+}
+
+export default function App() {
+  const [users] = useState({
+    users: ""
+  })
+
+  return(
+    <div className="app">
+    <UserForm />
+    <UserList />
+    </div>
+  )
+}
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       users: []
+//     };
+//   }
+
+//   fetchUsers() {
+//     axios
+//       .get("http://localhost:4000/api/users")
+//       .then(res => {
+//         this.setState({ users: res.data });
+//       })
+//       .catch(err => {
+//         this.setState({ error: err });
+//         console.log(err);
+//       });
+//   }
+
+//   deleteUser = (e, id) => {
+//     e.preventDefault();
+//     axios
+//       .delete(`http://localhost:4000/api/users/${id}`)
+//       .then(res => {
+//         this.fetchUsers();
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   };
+
+//   addUser = (name, bio) => {
+//     const newUser = {
+//       "name": name,
+//       "bio": bio
+//     };
+//     axios
+//     .post("http://localhost:4000/api/users", newUser)
+//     .then(res => {
+//         // console.log(res)
+//         // this.setState({ users: [...this.state.users, res.data]})
+//         this.fetchUsers();
+//     })
+//         .catch(err => {
+//         console.log(err);
+//       });
+   
+//   };
+
+//   updateUser = (name, bio, id) => {
+//     const updatedUser = {
+//       "name": name,
+//       "bio": bio
+//     };
+//     axios
+//       .put(`http://localhost:4000/api/users/${id}`, updatedUser)
+//       .then(res => {
+//         this.fetchUsers();
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+
+//   };
+
+//   componentDidMount() {
+//     this.fetchUsers();
+//   }  
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <UserForm addUser={this.addUser} updateUser={this.updateUser}/>
+//         <UserList users={this.state.users} deleteUser={this.deleteUser} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
